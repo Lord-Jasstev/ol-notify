1f571a3b19166760873dc00af05bc548d15d281b
Online Leviathan <onlineleviathan@gmail.com>
2018-09-13 16:17:03 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Simple Observer Pattern Framework for Nodejs
+## Completely untested!  Completely for fun and learning experience.
---
+++ index.js
@@ -0,0 +1,11 @@
+const Notifier = require('./lib/notifier')
+const EventsHandler = require('.lib/eventHandlers/eventsHandler')
+const Event = require('./lib/events/event')
+const EventHandler = require('./lib/eventHandlers/eventHandler')
+
+module.exports = {
+  Notifier,
+  EventsHandler,
+  Event,
+  EventHandler
+}
