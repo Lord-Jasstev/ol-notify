8f9e952b6b2309fd243eda0c439918708b79b369
Online Leviathan <onlineleviathan@gmail.com>
2018-09-14 13:21:26 -0400

    fix path name

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Notifier = require('./lib/notifier')
-const EventsHandler = require('.lib/eventHandlers/eventsHandler')
+const EventsHandler = require('./lib/eventHandlers/eventsHandler')
 const Event = require('./lib/events/event')
 const EventHandler = require('./lib/eventHandlers/eventHandler')
 
